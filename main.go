4c8cbd75e4eb347000a5acb69f314d072687935c
SnowSword1010 <jmayank2001@gmail.com>
2021-10-09 11:56:20 +0530

    :zap: Homepage end point added

--- main.go
+++ main.go
@@ -2,22 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
-	id       uint64 `json:"id"`
-	Name     string `json:"Name"`
-	Email    string `json:"Email"`
-	Password string `json:"Password"`
+	id       primitive.ObjectID `json:"_id" bson:"_id"`
+	Name     string             `json:"Name" bson:"Name"`
+	Email    string             `json:"Email" bson:"Email"`
+	Password string             `json:"Password" bson:"Password"`
 }
 
 type Posts struct {
-	id               uint64 `json:"id"`
-	Caption          string `json:"Caption"`
-	Image_URL        string `json:"Image_URL"`
-	Posted_Timestamp string `json:"Posted_Timestamp"`
+	id               primitive.ObjectID `json:"_id" bson:"_id"`
+	Caption          string             `json:"Caption" bson:"Caption"`
+	Image_URL        string             `json:"Image_URL" bson:"Image_URL"`
+	Posted_Timestamp string             `json:"Posted_Timestamp" bson:"Posted_Timestamp"`
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to Instagram Backend API | Appointy Task")
 }
 
 func main() {
-	fmt.Println("Hello")
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
