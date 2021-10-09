a1209440afa4061163755374776e7d25b9713172
SnowSword1010 <jmayank2001@gmail.com>
2021-10-09 22:37:55 +0530

    :bookmark: Added pagination | Implemented UserPost Endpoint

--- userPostEndPoint.go
+++ userPostEndPoint.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"reflect"
 	"strconv"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -26,6 +25,7 @@ func userPostEndPoint(w http.ResponseWriter, r *http.Request) {
 	// setting timeout for request
 	//ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	collection := client.Database("Instagram-Backend-API").Collection("posts")
+	collection2 := client.Database("Instagram-Backend-API").Collection("users")
 	if r.Method == "GET" {
 		uids, ok := r.URL.Query()["uid"]
 
@@ -33,16 +33,47 @@ func userPostEndPoint(w http.ResponseWriter, r *http.Request) {
 			log.Println("Url Param 'uid' is missing")
 			return
 		}
+
 		uid, _ := strconv.ParseInt(uids[0], 0, 64)
-		fmt.Println(reflect.TypeOf(uid))
+
+		// implementing pagination : showing 5 posts at a time
+		pages, notok := r.URL.Query()["page"]
+
+		if !notok || len(pages[0]) < 1 {
+			log.Println("Page Param is missing")
+			return
+		}
+
+		page, _ := strconv.ParseInt(pages[0], 0, 64)
+		var startindx, endindx int64 = page * 5, page*5 + 5
+
+		if startindx <= -1 || endindx <= -1 {
+			return
+		}
+
 		JSONData := struct {
-			UserId uint64 `bson:"User_id"`
-			Name   string `bson:"Name"`
-			Email  string `bson:"Email"`
+			UserId    uint64   `bson:"User_id"`
+			Name      string   `bson:"Name"`
+			Email     string   `bson:"Email"`
+			PostSlice []uint64 `json:"PostSlice" bson:"PostSlice"`
 		}{}
-		collection.FindOne(context.TODO(), bson.D{primitive.E{Key: "User_id", Value: uid}}).Decode(&JSONData)
-		fmt.Println(JSONData)
-		json.NewEncoder(w).Encode(JSONData)
+
+		collection2.FindOne(context.TODO(), bson.D{primitive.E{Key: "User_id", Value: uid}}).Decode(&JSONData)
+
+		mySlice := JSONData.PostSlice
+		JSONPost := struct {
+			Post_id          uint64 `bson:"Post_id"`
+			Caption          string `bson:"Caption"`
+			Image_URL        string `bson:"Image_URL"`
+			Posted_Timestamp string `bson:"Posted_Timestamp"`
+		}{}
+
+		for _, element := range mySlice {
+			fmt.Println(element)
+			collection.FindOne(context.TODO(), bson.D{primitive.E{Key: "Post_id", Value: element}}).Decode(&JSONPost)
+			json.NewEncoder(w).Encode(JSONPost)
+		}
+
 		// json.NewEncoder(w).Encode("Kindly make post requests on this URL to create new users.")
 	}
 }
